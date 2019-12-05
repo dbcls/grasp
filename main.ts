131b15e30280f154df28fcb7bb77dda336050c5e
Keita Urashima <ursm@ursm.jp>
2019-12-05 22:30:44 +0900

    An attempt to generate resolvers from a schema

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import Handlebars = require("handlebars");
 import { URLSearchParams } from "url";
 import { ApolloServer } from "apollo-server";
 import outdent from 'outdent';
+import gql from 'graphql-tag';
 
 /*
 次のようなクエリができる:
@@ -23,7 +24,7 @@ import outdent from 'outdent';
 */
 
 // スキーマをユーザに定義してもらう
-const schemaDoc = outdent`
+const schemaDoc = gql`
   type Query {
     Prefecture(name: String): Prefecture
   }
@@ -39,34 +40,35 @@ const schemaDoc = outdent`
   }
 `;
 
-const Prefecture = {
-  endpoint: "http://ja.dbpedia.org/sparql",
+const typeDefs = {
+  Prefecture: {
+    endpoint: "http://ja.dbpedia.org/sparql",
 
-  query: Handlebars.compile(outdent`
-    PREFIX prop-ja: <http://ja.dbpedia.org/property/>
-    PREFIX resource-ja: <http://ja.dbpedia.org/resource/>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    SELECT DISTINCT *
-    WHERE {
-      resource-ja:{{name}} rdfs:label ?name.
-      resource-ja:{{name}} prop-ja:花 ?flower.
-      resource-ja:{{name}} prop-ja:隣接都道府県 ?adjacentPrefecture.
-    }
-  `, {noEscape: true})
-};
+    query: Handlebars.compile(outdent`
+      PREFIX prop-ja: <http://ja.dbpedia.org/property/>
+      PREFIX resource-ja: <http://ja.dbpedia.org/resource/>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      SELECT DISTINCT *
+      WHERE {
+        resource-ja:{{name}} rdfs:label ?name.
+        resource-ja:{{name}} prop-ja:花 ?flower.
+        resource-ja:{{name}} prop-ja:隣接都道府県 ?adjacentPrefecture.
+      }
+    `, {noEscape: true})
+  },
+  Flower: {
+    endpoint: "http://ja.dbpedia.org/sparql",
 
-const Flower = {
-  endpoint: "http://ja.dbpedia.org/sparql",
-
-  query: Handlebars.compile(outdent`
-    PREFIX prop-ja: <http://ja.dbpedia.org/property/>
-    PREFIX resource-ja: <http://ja.dbpedia.org/resource/>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    SELECT DISTINCT *
-    WHERE {
-      <{{iri}}> rdfs:label ?name.
-    }
-  `, {noEscape: true})
+    query: Handlebars.compile(outdent`
+      PREFIX prop-ja: <http://ja.dbpedia.org/property/>
+      PREFIX resource-ja: <http://ja.dbpedia.org/resource/>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+      SELECT DISTINCT *
+      WHERE {
+        <{{iri}}> rdfs:label ?name.
+      }
+    `, {noEscape: true})
+  }
 };
 
 function mapValues(obj: object, fn: (val: any) => any): object {
@@ -99,34 +101,52 @@ async function queryFirstBinding(typeDef: {endpoint: string, query: (args: objec
   return bindings[0];
 }
 
+const query = schemaDoc.definitions.find(def => def.name.value === 'Query');
+const types = schemaDoc.definitions.filter(def => def.name.value !== 'Query');
+
 // クエリも定義する
 const root = {
-  Query: {
-    async Prefecture(_parent: object, {name}) {
-      return await queryFirstBinding(Prefecture, {name});
-    }
-  },
-  Prefecture: {
-    async adjacentPrefectures({name}) {
-      // const iri = `<http://ja.dbpedia.org/resource/${name}>`;
-      const bindings = await queryAllBindings(Prefecture, {name});
-
-      const queryPrefectures = bindings.map(async ({adjacentPrefecture: iri}) => {
-        const name = iri.split("/").slice(-1)[0];
-        return await queryFirstBinding(Prefecture, {name});
-      });
-
-      return await Promise.all(queryPrefectures);
-    },
-    async flower({flower}) {
-      // TODO ほんとはこれがIRIか判定したいんだけど雑にやってる
-      if (flower.startsWith("http")) {
-        return await queryFirstBinding(Flower, {iri: flower});
-      } else {
-        return { name: flower };
+  Query: query.fields.reduce((acc, field) => (
+    Object.assign(acc, {
+      [field.name.value]: async (_parent: object, args: object) => {
+        // TODO スキーマの型に応じて取り方を変える必要がある？
+        return await queryFirstBinding(typeDefs[field.name.value], args);
       }
-    }
-  }
+    })
+  ), {}),
+  ...types.reduce((acc, type) => {
+    return Object.assign(acc, {
+      [type.name.value]: type.fields.reduce((acc, field) => {
+        return Object.assign(acc, field.type.kind === 'ListType' ? {
+          [field.name.value]: async (args) => {
+            console.log(typeDefs[type.name.value])
+            const bindings = await queryAllBindings(typeDefs[type.name.value], args);
+
+            // TODO 汎化できていない
+            const queries = bindings.map(async ({adjacentPrefecture: iri}) => {
+              const args = {
+                name: iri.split("/").slice(-1)[0]
+              };
+
+              return await queryFirstBinding(typeDefs[field.type.type.name.value], args);
+            });
+
+            return await Promise.all(queries);
+          }
+        } : Object.keys(typeDefs).includes(field.type.name.value) ? {
+          [field.name.value]: async (args) => {
+            // TODO 汎化できていない
+            // TODO ほんとはこれがIRIか判定したいんだけど雑にやってる
+            if (args.flower.startsWith("http")) {
+              return await queryFirstBinding(typeDefs.Flower, {iri: args.flower});
+            } else {
+              return { name: args.flower };
+            }
+          }
+        } : {});
+      }, {})
+    })
+  }, {})
 };
 
 const port = process.env.PORT || 4000;
