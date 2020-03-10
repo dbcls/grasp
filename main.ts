35a97c66f90dcdc3835014e95ab6d191f11d6c14
Keita Urashima <ursm@ursm.jp>
2020-03-10 14:26:16 +0900

    Investigate

--- main.ts
+++ main.ts
@@ -95,7 +95,7 @@ SchemaLoader.loadFrom('./resources').then(loader => {
           acc.set(resource, new DataLoader(async (iris: ReadonlyArray<string>) => {
             return resource.fetchByIRIs(iris);
           }, {
-            maxBatchSize: 100
+            maxBatchSize: 10
           }));
         }, new Map<Resource, DataLoader<string, ResourceEntry | null>>())
       };
