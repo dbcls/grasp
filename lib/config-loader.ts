3519e4e4329fe91c0f866a026557b84f307584b5
Miel Vander Sande <miel.vandersande@meemoo.be>
2022-01-07 17:26:29 +0100

    DEV-1886 Refactors utils

--- lib/config-loader.ts
+++ lib/config-loader.ts
@@ -12,15 +12,16 @@ interface Service {
 }
 
 export default class ConfigLoader {
-
-  static async loadTemplateIndexFromDirectory(baseDir: string): Promise<Map<string, string>> {
+  static async loadTemplateIndexFromDirectory(
+    baseDir: string
+  ): Promise<Map<string, string>> {
     const templateIndex: Map<string, string> = new Map();
 
     for (const path of await readdir(baseDir)) {
       if (!/^[0-9a-zA-Z].*\.sparql$/.test(path)) {
         continue;
       }
-      
+
       const query = await readFile(join(baseDir, path), {
         encoding: "utf-8",
       });
@@ -29,13 +30,20 @@ export default class ConfigLoader {
     return templateIndex;
   }
 
-  static async loadServiceIndexFromFile(serviceFile: string): Promise<Map<string, SparqlClient>> {
-    
-    const services: {[key: string]: Service } = JSON.parse(
-      await readFile(serviceFile, {
+  static async loadServiceIndexFromFile(
+    serviceFile: string
+  ): Promise<Map<string, SparqlClient>> {
+
+    const jsonString = await readFile(serviceFile, {
         encoding: "utf-8",
-      })
-    );
+    });
+    return ConfigLoader.loadServiceIndexFromJsonString(jsonString);
+  }
+
+  static loadServiceIndexFromJsonString(
+    jsonString: string
+  ): Map<string, SparqlClient> {
+    const services: { [key: string]: Service } = JSON.parse(jsonString);
 
     return new Map(
       Object.keys(services).map((name) => {
--- lib/utils.ts
+++ lib/utils.ts
@@ -60,3 +60,15 @@ export function ensureArray<T>(obj: T | Array<T>): Array<T> {
     return obj ? [obj] : [];
   }
 }
+
+export function ntriplesLiteral(strs: string | string[]): string[] {
+  return ensureArray(strs).map((str) => `"${str}"`);
+}
+
+export function ntriplesIri(strs: string | string[]): string[] {
+  return ensureArray(strs).map((str) => `<${str}>`);
+}
+
+export function join(separator: string, strs: string | string[]): string {
+  return ensureArray(strs).join(separator);
+}
