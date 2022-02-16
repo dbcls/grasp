f3cd69545d115448b41fddda8ef9ba3019cb6472
Miel Vander Sande <miel.vandersande@meemoo.be>
2022-02-16 22:09:53 +0100

    DEV-1882 Complete unittests utils

--- test/utils-test.ts
+++ test/utils-test.ts
@@ -1,5 +1,19 @@
-import { ensureArray, oneOrMany, hasDirective, isListType } from "../lib/utils";
-import { ObjectTypeDefinitionNode, TypeNode } from "graphql";
+import {
+  ensureArray,
+  oneOrMany,
+  hasDirective,
+  isListType,
+  unwrapCompositeType,
+  getDirective,
+  getDirectiveArgumentValue,
+  valueToString,
+} from "../lib/utils";
+import {
+  ObjectTypeDefinitionNode,
+  TypeNode,
+  DirectiveNode,
+  ValueNode,
+} from "graphql";
 
 describe("utils", () => {
   describe("isListType", () => {
@@ -49,43 +63,118 @@ describe("utils", () => {
         const def: TypeNode = {
           kind: "NonNullType",
           type: {
+            kind: "NamedType",
+            name: {
+              kind: "Name",
+              value: "test",
+            },
+          },
+        };
+        return expect(isListType(def)).toBeFalsy();
+      });
+    });
+  });
+  describe("unwrapCompositeType", () => {
+    describe("with nested NamedType", () => {
+      it("should return NamedType", () => {
+        const def: TypeNode = {
+          kind: "ListType",
+          type: {
+            kind: "NamedType",
+            name: {
+              kind: "Name",
+              value: "test",
+            },
+          },
+        };
+        return expect(unwrapCompositeType(def)).toEqual({
+          kind: "NamedType",
+          name: {
+            kind: "Name",
+            value: "test",
+          },
+        });
+      });
+      it("should return NamedType when double nested", () => {
+        const def: TypeNode = {
+          kind: "NonNullType",
+          type: {
+            kind: "ListType",
+            type: {
               kind: "NamedType",
               name: {
                 kind: "Name",
                 value: "test",
               },
-
+            },
           },
         };
-        return expect(isListType(def)).toBeTruthy();
-      });      
+        return expect(unwrapCompositeType(def)).toEqual({
+          kind: "NamedType",
+          name: {
+            kind: "Name",
+            value: "test",
+          },
+        });
+      });
+      it("should return NamedType when nested in NonNullType", () => {
+        const def: TypeNode = {
+          kind: "NonNullType",
+          type: {
+            kind: "NamedType",
+            name: {
+              kind: "Name",
+              value: "test",
+            },
+          },
+        };
+        return expect(unwrapCompositeType(def)).toEqual({
+          kind: "NamedType",
+          name: {
+            kind: "Name",
+            value: "test",
+          },
+        });
+      });
+    });
+    describe("with not nested NamedType", () => {
+      it("should return itself", () => {
+        const def: TypeNode = {
+          kind: "NamedType",
+          name: {
+            kind: "Name",
+            value: "test",
+          },
+        };
+        return expect(unwrapCompositeType(def)).toBe(def);
+      });
     });
   });
   describe("ensureArray", () => {
     describe("with array", () => {
-      it("should return empty array", async () => {
+      it("should return empty array", () => {
         return expect(ensureArray([])).toEqual([]);
       });
 
-      it("should return empty array if array with null", async () => {
+      it("should return empty array if array with null", () => {
         return expect(ensureArray([null])).toEqual([null]);
       });
     });
 
     describe("with value", () => {
-      it("should return empty array if value null", async () => {
+      it("should return empty array if value null", () => {
         return expect(ensureArray(null)).toEqual([]);
       });
 
-      it("should return array with string if value string", async () => {
+      it("should return array with string if value string", () => {
         return expect(ensureArray("a")).toEqual(["a"]);
       });
 
-      it("should return array with empty object if value empty object", async () => {
+      it("should return array with empty object if value empty object", () => {
         return expect(ensureArray({})).toEqual([{}]);
       });
 
-      it("should return array with object if value object", async () => {
+      it("should return array with object if value object", () => {
         return expect(ensureArray({ a: "a" })).toEqual([{ a: "a" }]);
       });
     });
@@ -93,29 +182,29 @@ describe("utils", () => {
 
   describe("oneOrMany", () => {
     describe("with one", () => {
-      it("should return one if multi value", async () => {
+      it("should return one if multi value", () => {
         return expect(oneOrMany(["a", "b"], true)).toEqual("a");
       });
 
-      it("should return one if single value", async () => {
+      it("should return one if single value", () => {
         return expect(oneOrMany(["a"], true)).toEqual("a");
       });
 
-      it("should return undefined if empty array", async () => {
+      it("should return undefined if empty array", () => {
         return expect(oneOrMany([], true)).toEqual(undefined);
       });
     });
 
     describe("with many", () => {
-      it("should return one if multi value", async () => {
+      it("should return one if multi value", () => {
         return expect(oneOrMany(["a", "b"], false)).toEqual(["a", "b"]);
       });
 
-      it("should return one if single value", async () => {
+      it("should return one if single value", () => {
         return expect(oneOrMany(["a"], false)).toEqual(["a"]);
       });
 
-      it("should return empty array if empty array", async () => {
+      it("should return empty array if empty array", () => {
         return expect(oneOrMany([], false)).toEqual([]);
       });
     });
@@ -139,16 +228,112 @@ describe("utils", () => {
       ],
     };
 
-    it("should return true if directive is present", async () => {
+    it("should return true if directive is present", () => {
       return expect(hasDirective(def, "test")).toEqual(true);
     });
 
-    it("should return false if directive is not present", async () => {
+    it("should return false if directive is not present", () => {
       return expect(hasDirective(def, "test2")).toEqual(false);
     });
 
-    it("should return undefined if directive name is null", async () => {
+    it("should return undefined if directive name is null", () => {
       return expect(hasDirective(def, null)).toEqual(false);
+    });
+  });
+
+  describe("getDirective", () => {
+    const def: ObjectTypeDefinitionNode = {
+      kind: "ObjectTypeDefinition",
+      name: {
+        kind: "Name",
+        value: "definition",
+      },
+      directives: [
+        {
+          kind: "Directive",
+          name: {
+            kind: "Name",
+            value: "test",
+          },
+        },
+      ],
+    };
+
+    it("should return DirectiveNode if directive is present", () => {
+      return expect(getDirective(def, "test")).toEqual({
+        kind: "Directive",
+        name: {
+          kind: "Name",
+          value: "test",
+        },
+      });
+    });
+
+    it("should return undefined if directive is not present", () => {
+      return expect(getDirective(def, "test2")).toBeUndefined();
+    });
+
+    it("should return undefined if directive name is null", () => {
+      return expect(getDirective(def, null)).toBeUndefined();
+    });
+  });
+
+  describe("getDirectiveArgumentValue", () => {
+    const def: DirectiveNode = {
+      kind: "Directive",
+      name: {
+        kind: "Name",
+        value: "name",
+      },
+      arguments: [
+        {
+          kind: "Argument",
+          name: {
+            kind: "Name",
+            value: "name",
+          },
+          value: {
+            kind: "StringValue",
+            value: "test",
+          },
+        },
+      ],
+    };
+
+    it("should return value if argument is present", () => {
+      return expect(getDirectiveArgumentValue(def, "name")).toBe("test");
+    });
+
+    it("should return undefined if argument is not present", () => {
+      return expect(getDirectiveArgumentValue(def, "name2")).toBeUndefined();
+    });
+
+    it("should return undefined if argument name is null", () => {
+      return expect(getDirectiveArgumentValue(def, null)).toBeUndefined();
+    });
+  });
+
+  describe("valueToString", () => {
+    it("should return value if of type StringValue", () => {
+      return expect(
+        valueToString({
+          kind: "StringValue",
+          value: "test",
+        })
+      ).toBe("test");
+    });
+
+    it("should return undefined if not of type StringValue ", () => {
+      return expect(
+        valueToString({
+          kind: "BooleanValue",
+          value: true,
+        })
+      ).toBeUndefined()
+    });
+
+    it("should return undefined if argument name is null", () => {
+      return expect(valueToString(null)).toBeUndefined();
     });
   });
 });
