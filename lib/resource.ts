c74c5e11c1c7774134bf02f26f96829ff6b984ee
Miel Vander Sande <miel.vandersande@meemoo.be>
2022-03-15 13:59:42 +0100

    Replace node-cache with LRU-cache

--- lib/resource.ts
+++ lib/resource.ts
@@ -18,14 +18,14 @@ import {
   ntriplesLiteral,
 } from "./utils";
 import SparqlClient from "sparql-http-client";
-import NodeCache from "node-cache";
+import LRU from "lru-cache";
 import logger from "./logger";
 
 type CompiledTemplate = (args: object) => string;
 export type ResourceEntry = Record<string, any>;
 
 const NS_REGEX = /^https:\/\/github\.com\/dbcls\/grasp\/ns\//;
-const DEFAULT_TTL = 100;
+const DEFAULT_TTL = 1000 * 60 * 1;
 
 // Create handlebars compiler
 const handlebars = Handlebars.create();
@@ -34,11 +34,12 @@ handlebars.registerHelper("as-iriref", ntriplesIri);
 handlebars.registerHelper("as-string", ntriplesLiteral);
 
 // Create data cache
-let ttl: number = process.env.QUERY_CACHE_TTL
-  ? parseInt(process.env.QUERY_CACHE_TTL, 10)
-  : DEFAULT_TTL;
-ttl = isNaN(ttl) ? ttl : DEFAULT_TTL;
-const cache = new NodeCache({ stdTTL: ttl, checkperiod: 2 * ttl });
+const options = {
+    max: parseInt(process.env.CACHE_SIZE || '20', 10),
+    ttl: parseInt(process.env.CACHE_SIZE || `${DEFAULT_TTL}`, 10)
+  }
+
+const cache = new LRU<string, Quad[]>(options);
 
 function buildEntry(
   bindingsGroupedBySubject: Record<string, Quad[]>,
@@ -91,10 +92,6 @@ function buildEntry(
 }
 
 export default class Resource {
-  static cache = new NodeCache({
-    stdTTL: DEFAULT_TTL,
-    checkperiod: 2 * DEFAULT_TTL,
-  });
   resources: Resources;
   definition: ObjectTypeDefinitionNode;
   sparqlClient?: SparqlClient;
