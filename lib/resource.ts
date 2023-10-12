ba7e77f99477d5fa99ce998106642c6e1c3ed720
Miel Vander Sande <miel.vandersande@meemoo.be>
2023-10-12 13:42:46 +0200

    Move processing functions to separate file.

--- lib/resource.ts
+++ lib/resource.ts
@@ -1,14 +1,8 @@
 import Handlebars from "handlebars";
-import type { Quad, Stream } from "@rdfjs/types";
-import { getTermRaw } from "rdf-literal";
-import transform from "lodash/transform.js";
 import { ObjectTypeDefinitionNode } from "graphql";
 
 import Resources from "./resources.js";
 import {
-  oneOrMany,
-  isListType,
-  unwrapCompositeType,
   hasDirective,
   getDirective,
   getDirectiveArgumentValue,
@@ -19,14 +13,11 @@ import {
 import SparqlClient from "sparql-http-client";
 import { LRUCache } from "lru-cache";
 import logger from "./logger.js";
-import { Dictionary } from "lodash";
-import internal, { Readable } from "stream"
-import { query } from 'express'
+import { buildEntry, fetchResultsUntilThreshold, groupBindingsStream } from './resource-util.js'
 
 type CompiledTemplate = (args: object) => string;
 export type ResourceEntry = Record<string, any>;
 
-const NS_REGEX = /^https:\/\/github\.com\/dbcls\/grasp\/ns\//;
 const DEFAULT_TTL = 1000 * 60 * 1;
 
 // Create handlebars compiler
@@ -42,152 +33,6 @@ const options = {
 };
 
 const cache = new LRUCache<string, ResourceEntry[]>(options);
-
-export function buildEntry(
-  bindingsGroupedBySubject: Record<string, Quad[]>,
-  subject: string,
-  resource: Resource,
-  resources: Resources
-): ResourceEntry {
-  const entry: ResourceEntry = {};
-
-  // Turn the resulting Quads into records
-  const pValues = transform(
-    bindingsGroupedBySubject[subject],
-    (acc, { predicate, object }: Quad) => {
-      // Extract property name from URI
-      const k = predicate.value.replace(NS_REGEX, "");
-
-      // Converts any RDF term to a JavaScript primitive.
-      const v: any = getTermRaw(object);
-
-      // If property is not yet in the record accumulator, then initialise with empty array
-      // Push object value into array
-      (acc[k] || (acc[k] = [])).push(v);
-    },
-    {} as Record<string, string[]>
-  );
-
-  // Resolve any non-scalar types
-  (resource.definition.fields || []).forEach((field) => {
-    const type = field.type;
-    const name = field.name.value;
-    const values = pValues[name] || [];
-
-    // Get the type
-    const targetType = unwrapCompositeType(type);
-    // Find the corresponding resource
-    const targetResource = resources.lookup(targetType.name.value);
-
-    // If the resource is embedded, build entries from exiting bindings
-    if (targetResource?.isEmbeddedType) {
-      const entries = values.map((nodeId) =>
-        buildEntry(bindingsGroupedBySubject, nodeId, targetResource, resources)
-      );
-      entry[name] = oneOrMany(entries, !isListType(type));
-    } else {
-      entry[name] = oneOrMany(values, !isListType(type));
-    }
-  });
-
-  // Make sure entries always have an iri
-  if (!entry.iri)
-    entry.iri = subject
-  return entry;
-}
-
-export async function groupBindingsStream(stream: Stream<Quad>): Promise<{
-  bindingsGroupedBySubject: Dictionary<Quad[]>;
-  primaryBindingsGroupedBySubject: Dictionary<Quad[]>;
-}> {
-  return new Promise((resolve) => {
-    const bindingsGroupedBySubject: Dictionary<Quad[]> = {};
-    const primaryBindingsGroupedBySubject: Dictionary<Quad[]> = {};
-
-    stream.on("data", (binding: Quad) => {
-      // Group all bindings by subject
-      bindingsGroupedBySubject[binding.subject.value] =
-        bindingsGroupedBySubject[binding.subject.value] || [];
-      bindingsGroupedBySubject[binding.subject.value].push(binding);
-      // Remove BlankNodes from bindings
-      if (binding.subject.termType !== "BlankNode") {
-        // Group the primaryBindings by subject value
-        primaryBindingsGroupedBySubject[binding.subject.value] =
-          primaryBindingsGroupedBySubject[binding.subject.value] || [];
-        primaryBindingsGroupedBySubject[binding.subject.value].push(binding);
-      }
-    });
-    stream.on("end", () => {
-      resolve({
-        bindingsGroupedBySubject,
-        primaryBindingsGroupedBySubject,
-      });
-    });
-    stream.on("error", (err: any) => {
-      throw new Error(`Cannot process SPARQL endpoint results: ${err}`);
-    });
-  });
-}
-
-export async function fetchResultsUntilThreshold(
-  sparqlClient: SparqlClient,
-  sparqlQuery: string,
-  threshold: number, options?: SparqlClient.QueryOptions
-): Promise<Stream<Quad> & internal.Readable> {
-  // If the threshold is 0 or lower, just execute the query without paging
-  if (threshold <= 0) {
-    return sparqlClient.query.construct(
-      sparqlQuery,
-        options
-    )
-  }
-  return new Readable({
-      objectMode: true,
-      async read() {
-          const self = this
-          async function fetchBindings(sparqlClient: SparqlClient,
-              pagedQuery: string,
-              offset: number = 0) {
-
-              // Fetch all bindings
-              const bindingsStream = await sparqlClient.query.construct(
-                pagedQuery,
-                  options
-              )
-
-              let count = 0
-              bindingsStream.on('data', (q: Quad) => {
-                  self.push(q)
-                  count++
-              })
-              bindingsStream.on('end', () => {
-                  if (count === threshold) {
-                      // Emit new results
-                      offset += threshold
-                      // Alter query
-                      const alteredQuery = `${sparqlQuery} 
-                      OFFSET ${offset}
-                      LIMIT ${threshold}`
-                      
-                      // Repeat the process
-                      fetchBindings(sparqlClient, alteredQuery, offset)
-                  } else {
-                    // If fewer than threshold results are returned, end the stream
-                    self.push(null)
-                  }
-              })
-
-          }
-
-          // Implement the logic to fetch and emit results here
-          try {
-              fetchBindings(sparqlClient, sparqlQuery)
-          } catch (error) {
-              this.emit("error", error)
-          }
-      },
-  })
-}
 
 export default class Resource {
   resources: Resources;
---
+++ test/resource-util.ts
@@ -0,0 +1,237 @@
+import {
+    buildEntry,
+    fetchResultsUntilThreshold
+} from "../lib/resource-util.js"
+
+import {
+    getTestResource,
+    getTestResources,
+    getTestSparqlClient,
+} from "./test-helpers.js"
+import type { Quad } from "@rdfjs/types"
+// @ts-ignore
+import quad from "rdf-quad"
+
+
+describe("fetchResultsUntilThreshold", () => {
+
+    const subject = "http://example.org/subject1"
+    const subject2 = "http://example.org/subject2"
+
+    const triples = [
+        quad(subject, "https://github.com/dbcls/grasp/ns/iri", subject),
+        quad(subject, "https://github.com/dbcls/grasp/ns/id", '"subject1"'),
+        quad(subject, "https://github.com/dbcls/grasp/ns/count", 5),
+        quad(subject, "https://github.com/dbcls/grasp/ns/test", true),
+        quad(subject, "https://github.com/dbcls/grasp/ns/obsolete", "obsolete"),
+        quad(subject2, "https://github.com/dbcls/grasp/ns/iri", subject2),
+        quad(subject2, "https://github.com/dbcls/grasp/ns/id", '"subject2"'),
+        quad(subject2, "https://github.com/dbcls/grasp/ns/count", 4),
+        quad(subject2, "https://github.com/dbcls/grasp/ns/test", false),
+        quad("_:b1", "https://github.com/dbcls/grasp/ns/iri", subject),
+        quad("_:b1", "https://github.com/dbcls/grasp/ns/id", '"subject"'),
+    ]
+    const threshold = 5
+    const sparqlClient = getTestSparqlClient(triples, threshold)
+
+    it("should not throw", async () => {
+        expect(await fetchResultsUntilThreshold(sparqlClient, "SELECT * WHERE { ?s ?p ?o }", threshold)).not.toThrow()
+    })
+
+    it("should return stream", async () => {
+        const bindingsStream = await fetchResultsUntilThreshold(sparqlClient, "SELECT * WHERE { ?s ?p ?o }", threshold)
+
+        const actual: Array<Quad> = []
+        bindingsStream.on('data', (q: Quad) => {
+            actual.push(q)
+        })
+        bindingsStream.on('end', () => {
+            expect(actual.length).toEqual(triples.length)
+        })
+    })
+})
+
+describe("buildEntry", () => {
+    describe("called with values", () => {
+        const subject = "http://example.org/subject"
+        const bindingGroupedBySubject: Record<string, Quad[]> = {
+            [subject]: [
+                quad(subject, "https://github.com/dbcls/grasp/ns/iri", subject),
+                quad(subject, "https://github.com/dbcls/grasp/ns/id", '"subject"'),
+            ],
+        }
+        const res = getTestResource("assets/with-docs.graphql")
+        const resources = getTestResources(res)
+
+        it("should return ResourceEntry", () => {
+            return expect(
+                buildEntry(bindingGroupedBySubject, subject, res, resources)
+            ).toStrictEqual({
+                iri: subject,
+                id: "subject",
+            })
+        })
+    })
+
+    describe("called without iri", () => {
+        const subject = "http://example.org/subject"
+        const bindingGroupedBySubject: Record<string, Quad[]> = {
+            [subject]: [
+                quad(subject, "https://github.com/dbcls/grasp/ns/test1", '"Test"'),
+                quad(subject, "https://github.com/dbcls/grasp/ns/test2", "http://example.org/X"),
+                quad(subject, "https://github.com/dbcls/grasp/ns/test3", "http://example.org/Y")
+            ],
+        }
+        const res = getTestResource("assets/with-docs.graphql")
+        const resources = getTestResources(res)
+
+        it("should return ResourceEntry", () => {
+            return expect(
+                buildEntry(bindingGroupedBySubject, subject, res, resources)
+            ).toStrictEqual({
+                iri: subject,
+                id: undefined,
+            })
+        })
+    })
+
+    describe("called with embedded type", () => {
+        const subject = "http://example.org/subject"
+        const publisher = "http://example.org/publisher"
+        const bindingGroupedBySubject: Record<string, Quad[]> = {
+            [subject]: [
+                quad(subject, "https://github.com/dbcls/grasp/ns/iri", subject),
+                quad(
+                    subject,
+                    "https://github.com/dbcls/grasp/ns/publisher",
+                    publisher
+                ),
+            ],
+            [publisher]: [
+                quad(
+                    publisher,
+                    "https://github.com/dbcls/grasp/ns/name_ja",
+                    '"name_ja"'
+                ),
+                quad(
+                    publisher,
+                    "https://github.com/dbcls/grasp/ns/name_en",
+                    '"name_en"'
+                ),
+                quad(
+                    publisher,
+                    "https://github.com/dbcls/grasp/ns/page",
+                    '"publisher_page"'
+                ),
+            ],
+        }
+        const res = getTestResource("assets/with-embedded.graphql")
+        const emRes = getTestResource(
+            "assets/with-embedded.graphql",
+            "Publisher"
+        )
+        const resources = {
+            all: [res, emRes],
+            root: [res],
+            isUserDefined: () => true,
+            lookup: (name: string) => {
+                switch (name) {
+                    case "Publisher":
+                        return emRes
+                    case "Test":
+                    default:
+                        return res
+                }
+            },
+        }
+
+        it("should return ResourceEntry for embedded type", () => {
+            return expect(
+                buildEntry(bindingGroupedBySubject, publisher, emRes, resources)
+            ).toStrictEqual({
+                iri: publisher,
+                name_en: "name_en",
+                name_ja: "name_ja",
+                page: "publisher_page",
+            })
+        })
+
+        it("should return nested ResourceEntry for root type", () => {
+            return expect(
+                buildEntry(bindingGroupedBySubject, subject, res, resources)
+            ).toStrictEqual({
+                iri: subject,
+                publisher: {
+                    iri: publisher,
+                    name_en: "name_en",
+                    name_ja: "name_ja",
+                    page: "publisher_page",
+                },
+            })
+        })
+    })
+
+    describe("called with blanknode embedded type", () => {
+        const subject = "http://example.org/subject"
+        const bindingGroupedBySubject: Record<string, Quad[]> = {
+            [subject]: [
+                quad(subject, "https://github.com/dbcls/grasp/ns/iri", subject),
+                quad(
+                    subject,
+                    "https://github.com/dbcls/grasp/ns/publisher",
+                    '_:b1'
+                ),
+            ],
+            ['b1']: [
+                quad(
+                    '_:b1',
+                    "https://github.com/dbcls/grasp/ns/name_ja",
+                    '"name_ja"'
+                ),
+                quad(
+                    '_:b1',
+                    "https://github.com/dbcls/grasp/ns/name_en",
+                    '"name_en"'
+                ),
+                quad(
+                    '_:b1',
+                    "https://github.com/dbcls/grasp/ns/page",
+                    '"publisher_page"'
+                ),
+            ],
+        }
+        const res = getTestResource("assets/with-embedded.graphql")
+        const emRes = getTestResource(
+            "assets/with-embedded.graphql",
+            "Publisher"
+        )
+        const resources = {
+            all: [res, emRes],
+            root: [res],
+            isUserDefined: () => true,
+            lookup: (name: string) => {
+                switch (name) {
+                    case "Publisher":
+                        return emRes
+                    case "Test":
+                    default:
+                        return res
+                }
+            },
+        }
+
+        it("should return nested ResourceEntry for root type", () => {
+            return expect(
+                buildEntry(bindingGroupedBySubject, subject, res, resources)
+            ).toStrictEqual({
+                iri: subject,
+                publisher: {
+                    iri: "b1",
+                    name_en: "name_en",
+                    name_ja: "name_ja",
+                    page: "publisher_page",
+                },
+            })
+        })
+    })
+})
